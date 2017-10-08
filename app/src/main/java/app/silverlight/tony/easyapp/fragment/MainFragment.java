59e6ebdc5cb8bf38920e755d4e73638c9f80c6e2
TonySilverlight <woramet.m@gmail.com>
2017-10-08 11:07:54 +0700

    Main Fragment Edit

--- app/src/main/java/app/silverlight/tony/easyapp/fragment/MainFragment.java
+++ app/src/main/java/app/silverlight/tony/easyapp/fragment/MainFragment.java
@@ -3,9 +3,12 @@ package app.silverlight.tony.easyapp.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.zip.Inflater;
 
@@ -17,10 +20,79 @@ import app.silverlight.tony.easyapp.R;
 
 public class MainFragment extends Fragment{
 
+    //Explicit
+    private ImageView imageView;
+    private Button decButton, incButton;
+
+    //My constant
+    private int[] imageInts = new int[]{
+            R.drawable.icon1,
+            R.drawable.icon2,
+            R.drawable.icon3};
+    private int indexAnInt = 0;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main,container,false);
         return view;
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Initial View
+        initialView();
+//        Increase Controller
+        increaseController();
+//        Decrease Controller
+        decreaseController();
+
+    }
+
+    private void decreaseController() {
+        decButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(indexAnInt <= 0){
+                    indexAnInt = imageInts.length - 1;
+                }else {
+                    indexAnInt -= 1;
+                }
+                imageView.setImageResource(imageInts[indexAnInt]);
+            }
+        });
+    }
+
+    private void increaseController() {
+
+        final String tag = "80CTV1";
+
+        incButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(tag, "Length of Array  ==> " + imageInts.length);
+                if (indexAnInt < imageInts.length - 1) {
+                    //Todo
+                    indexAnInt += 1;
+                } else {
+                    indexAnInt = 0;
+
+                }
+                Log.d(tag,"Current index ==>" + indexAnInt);
+                imageView.setImageResource(imageInts[indexAnInt]);
+            } //Onclick
+        });
+    }
+
+    private void initialView() {
+        imageView = getView().findViewById(R.id.imvImage);
+        decButton = getView().findViewById(R.id.btnDecrease);
+        incButton = getView().findViewById(R.id.btnIncrease);
+
+
+    }
+
+
 } //Main Class
